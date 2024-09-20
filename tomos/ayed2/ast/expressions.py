b1a7111da46acf5e899b062ca83396b4dab7f776
Javier Mansilla <javimansilla@gmail.com>
2024-09-20 17:25:20 -0300

    moved operation expressions to file operators.

--- tomos/ayed2/ast/expressions.py
+++ tomos/ayed2/ast/expressions.py
@@ -14,14 +14,6 @@
 from tomos.ayed2.ast.types import *
 
 
-NAMED_CONSTANTS = [
-    "inf",
-    "null",
-    "true",
-    "false",
-]
-
-
 class Expr:
     pass
 
@@ -61,45 +53,6 @@ class Variable(Expr):
 
     def __repr__(self) -> str:
         return f"Variable({self.name})"
-
-
-class UnaryOp(Expr):
-    def __init__(self, op, expr):
-        self._op_token = op
-        self._expr = expr
-
-    @property
-    def op(self):
-        return self._op_token.value
-
-    @property
-    def expr(self):
-        return self._expr
-
-    def __repr__(self) -> str:
-        return f"UnaryOp({self.op}, {self.expr})"
-
-
-class BinaryOp(Expr):
-    def __init__(self, left, op, right):
-        self._left_expr = left
-        self._op_token = op
-        self._right_expr = right
-
-    @property
-    def left(self):
-        return self._left_expr
-
-    @property
-    def op(self):
-        return self._op_token.value
-
-    @property
-    def right(self):
-        return self._right_expr
-
-    def __repr__(self) -> str:
-        return f"BinaryOp({self.left}, {self.op}, {self.right})"
 
 
 class FunctionCall(Expr):
