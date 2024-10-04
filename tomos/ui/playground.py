200feca0b705721645eaec39599b7cbc38e26e93
Javier Mansilla <javimansilla@gmail.com>
2024-10-04 12:57:04 -0300

    addapted playground script to use docopt, and exporting options to the command line

--- tomos/ui/playground.py
+++ tomos/ui/playground.py
@@ -1,26 +1,49 @@
+"""
+
+Usage: playground.py [options] <source>
+
+Options:
+    --run           Runs the program
+    --step          Steps the program [default: False]
+    --delay <n>     Delay in seconds [default: 0.5]
+    --showast       Show the abstract syntax tree
+    --help          Show this message and exit
+"""
+from sys import exit
+from docopt import docopt
+
+from tomos.ayed2.parser import parser
+from tomos.ayed2.evaluation.interpreter import Interpreter
 from tomos.ui.interpreter_hooks import ShowSentence, ShowState, Sleeper, wait_for_input
 
 
 if __name__ == "__main__":
-    import sys
-    from tomos.ayed2.parser import parser
-    from tomos.ayed2.evaluation.interpreter import Interpreter
+    opts = docopt(__doc__)  # type: ignore
 
-    source = sys.argv[1]
+    source = opts["<source>"]
     ast = parser.parse(open(source).read())
-    print(ast.pretty())
 
-    interpreter = Interpreter(ast,
-                              pre_hooks=[ShowSentence(source, full=True),
-                                        #  Sleeper(0.5),
-                                         wait_for_input,
-                                         ],
-                              post_hooks=[ShowState('state.mem'), ])
-    if "--run" in sys.argv:
-        result = interpreter.run()
-        print(result)
-    elif "-i" in sys.argv:
-        print (sys.argv)
-        import ipdb; ipdb.set_trace()
+    if opts["--showast"]:
+        print(ast.pretty())
 
+    if opts["--run"]:
+        delay = opts["--delay"]
+        try:
+            delay = float(delay)
+        except ValueError:
+            print(f"Invalid delay: {delay}")
+            exit(1)
+        pre_hooks = [
+            ShowSentence(source, full=True),
+            Sleeper(delay)
+        ]
+
+        if opts["--step"]:
+            pre_hooks.append(wait_for_input)
+
+        interpreter = Interpreter(ast,
+                                  pre_hooks=pre_hooks,
+                                  post_hooks=[ShowState('state.mem'), ])
+        final_state = interpreter.run()
+        print(final_state)
 
