897815e6ab441e716e8a5491e115a47dda15862c
Javier Mansilla <javimansilla@gmail.com>
2024-10-23 13:33:16 -0300

    adding box for stack and heap, sizing vars and texts centered, arranging color scheme for data-types

--- tomos/ui/movie/panel/memory.py
+++ tomos/ui/movie/panel/memory.py
@@ -1,20 +1,25 @@
-from manim import VGroup, Rectangle, Text
+from manim import VGroup, Rectangle, RoundedRectangle, Text, BOLD
 from manim import LEFT, RIGHT, DOWN, UP
 
 colors = {
-    "": "#000000",
-    "white": "#ffffff",
-    "red": "#ff0000",
-    "blue": "#0000ff",
-    "IntType": "#00ff00",
-    "orange": "#ffa500",
-    "purple": "#800080"}
-
+    "IntType": "#A2D2DF",
+    "BoolType": "#F6EFBD",
+    "RealType": "##E4C087",
+    "CharType": "#BC7C7C",
+    }
 
 unamed_colors = ["red", "blue", "orange", "purple", "white"]
 
 
 class Variable(VGroup):
+    box_scale = 2
+    box_1_char_ratio = (0.4, 0.4)
+    box_extra_char_ratio = (0.2, 0)  # to be added to box_1_char_ratio per extra char
+    font_size_base = 12
+
+    @property
+    def font_size(self):
+        return self.font_size_base * self.box_scale
 
     def __init__(self, name, _type, value, **kwargs):
         super().__init__(**kwargs)
@@ -23,34 +28,61 @@ class Variable(VGroup):
             self.color = colors[str(_type)]
         else:
             self.color = unamed_colors.pop()
-        self.rect = Rectangle(width=1, height=0.25, fill_color=self.color, fill_opacity=0.5)
-        self.name = Text(name, font="Monospace").scale(0.2)
+
+        self.rect = self.build_box()
         self.add(self.rect)
+
+        self.name = Text(name, font="Monospace", font_size=self.font_size, weight=BOLD)
         self.name.align_to(self.rect, LEFT)
         self.name.align_to(self.rect, UP)
+        self.name.shift(UP * 0.2 * self.box_scale)
+
         self.add(self.name)
-        self.name.shift(UP * 0.15)
-        self.set_value(value)
+        self.value = self.build_value_text(value)
         self.add(self.value)
 
-    def set_value(self, value):
-        self.value = Text(str(value), font="Monospace").scale(0.2)
-        self.value.align_to(self.rect, LEFT)
-        self.value.align_to(self.rect, UP)
-        self.value.shift(DOWN * self.rect.height * 0.4)
-        self.value.shift(RIGHT * 0.2)
+    def build_box(self, extra_chars=0):
+        w, h = Variable.box_1_char_ratio
+        if extra_chars > 0:
+            w += extra_chars * Variable.box_extra_char_ratio[0]
+            h += extra_chars * Variable.box_extra_char_ratio[1]
+        w *= Variable.box_scale
+        h *= Variable.box_scale
+        rect = RoundedRectangle(
+            width=w, height=h,
+            fill_color=self.color, fill_opacity=0.25,
+            stroke_width=1, stroke_color=self.color,
+            corner_radius=0.1)
+        return rect
+
+    def build_value_text(self, value):
+        new_value = Text(str(value), font="Monospace", font_size=self.font_size)
+
+        # moves value to be in the center of the box
+        rect_h, rect_w = self.rect.height, self.rect.width
+        val_h, val_w = new_value.height, new_value.width
+        new_value.align_to(self.rect, LEFT)
+        new_value.align_to(self.rect, UP)
+        new_value.shift(DOWN * (rect_h/2 - val_h/2))
+        new_value.shift(RIGHT * (rect_w/2 - val_w/2))
+        return new_value
 
 
 class MemoryBlock(VGroup):
 
     def __init__(self, scene, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.space = Rectangle(width=7, height=4, color="black", fill_color="white", fill_opacity=0)
-        self.add(self.space)
-        self.space.next_to(self, RIGHT)
+        self.stack_blackboard = Rectangle(width=3, height=7, color="black", fill_color="white", fill_opacity=0.1)
+        self.heap_blackboard = Rectangle(width=3, height=7, color="black", fill_color="YELLOW_D", fill_opacity=0.1)
+        assert self.stack_blackboard.width == 3
+        self.stack_blackboard.move_to((1.5, 0, 0))
+
+        self.add(self.stack_blackboard)
+        self.add(self.heap_blackboard)
+        self.heap_blackboard.next_to(self.stack_blackboard, RIGHT)
+
         self.vars_by_name = {}
         self.last_block = None
-        self.animator = scene
 
     def process_snapshot(self, snapshot):
         print('PROCESSING SNAP')
@@ -67,21 +99,25 @@ class MemoryBlock(VGroup):
 
     def add_var(self, name, _type, value):
         var = Variable(name, _type, value)
-        var.align_to(self.space, LEFT)
+        var.align_to(self.stack_blackboard, LEFT)
         if self.last_block is None:
-            var.align_to(self.space, UP)
+            var.align_to(self.stack_blackboard, UP)
         else:
             var.align_to(self.last_block, DOWN)
             var.shift(DOWN * var.rect.height * 2.5)
-        # var.shift(RIGHT * 0.25)
-        self.space.add(var)
+        self.stack_blackboard.add(var)
         self.last_block = var
         self.vars_by_name[name] = var
 
-    def set_value(self, name, value):
+    def set_value(self, name, value, animator=None):
         var = self.vars_by_name[name]
-        if hasattr(var, 'value'):
-            var.remove(var.value)
-        var.set_value(value)
-        var.add(var.value)
+        new_value = var.build_value_text(value)
+        if animator is None:
+            if hasattr(var, 'value'):
+                var.remove(var.value)
+            var.value = new_value
+            var.add(var.value)
+        else:
+            animator(var, var.value, new_value)
+            var.add(var.value)
 
