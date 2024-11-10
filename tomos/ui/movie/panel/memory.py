5f5df229a9bcf7a044d95ff93231c8f684d83eb9
Javier Mansilla <javimansilla@gmail.com>
2024-11-10 15:05:10 -0300

    memory block drawn with skitso instead of manim

--- tomos/ui/movie/panel/memory.py
+++ tomos/ui/movie/panel/memory.py
@@ -1,39 +1,67 @@
-from manim import VGroup, Write, Unwrite, Rectangle, BOLD
-from manim import LEFT, RIGHT, DOWN, UP
+from skitso.atom import Container, Point
+from skitso import movement
+from skitso.shapes import Rectangle
 
 from tomos.ayed2.ast.types import PointerOf
 from tomos.ayed2.evaluation.state import MemoryAddress
 
+from tomos.ui.movie import configs
 from tomos.ui.movie.texts import build_text
 from tomos.ui.movie.panel.vars import Variable, PointerVar
 
 
-class MemoryBlock(VGroup):
+board_width, board_height = configs.MEMORY_BOARD_SIZE
+padding = configs.PADDING
 
-    def __init__(self, scene, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.scene = scene
-        self.stack_blackboard = Rectangle(width=3, height=7, color="black",
-                                          fill_color="white", fill_opacity=0.1)
-        self.heap_blackboard = Rectangle(width=3, height=7, color="black",
-                                         fill_color="YELLOW_D", fill_opacity=0.1)
-        assert self.stack_blackboard.width == 3
-        self.stack_blackboard.move_to((1.5, 0, 0))
+
+class Blackboard(Container):
+    def __init__(self, x, y, fill_color):
+        position = Point(x, y)
+        super().__init__(position)
+        self.rect = Rectangle(x, y, board_width, board_height, fill_color=fill_color,
+                             stroke_color="gray", stroke_width=3)
+        self.add(self.rect)
+        self.last_block = []
+
+    def add_var(self, var):
+        var.to_edge(self, movement.LEFT_EDGE)
+        var.shift(movement.RIGHT * padding)
+
+        if not self.last_block:
+            var.to_edge(self, movement.TOP_EDGE)
+            var.shift(movement.DOWN * padding)
+        else:
+            var.to_edge(self.last_block[-1], movement.BOTTOM_EDGE)
+            var.shift(movement.DOWN * (var.box_height + padding))
+        self.last_block.append(var)
+        self.add(var)
+
+
+class MemoryBlock(Container):
+
+    def __init__(self):
+        super().__init__(Point(0, 0))  # placed at origin. Will be shifted later.
+
+        title_size = configs.BASE_FONT_SIZE * 1.5
+        stack_title = build_text("STACK", font_size=title_size, bold=True)
+        self.add(stack_title)
+        heap_title = build_text("HEAP", font_size=title_size, bold=True)
+        heap_title.shift(movement.RIGHT * (board_width + padding))
+        self.add(heap_title)
+
+        boards_y = stack_title.box_height + padding
+        self.stack_blackboard = Blackboard(0, boards_y, fill_color="#3B1C32")
+        self.heap_blackboard = Blackboard(
+            heap_title.position.x, boards_y, fill_color="#6A1E55"
+        )
 
         self.add(self.stack_blackboard)
         self.add(self.heap_blackboard)
-        self.heap_blackboard.next_to(self.stack_blackboard, RIGHT)
-        stack_title = build_text("STACK", weight=BOLD)
-        heap_title = build_text("HEAP", weight=BOLD)
-        self.add(stack_title, heap_title)
-        stack_title.next_to(self.stack_blackboard, UP * .5)
-        heap_title.next_to(self.heap_blackboard, UP * .5)
 
         self.vars_by_name = {}  # the index of the vars in the memory
-        self.last_block = {}
 
     def process_snapshot(self, snapshot):
-        print('PROCESSING SNAP')
+        print('PROCESSING SNAPSHOT')
         for name_or_addr in snapshot.diff.new_cells:
             print("Adding", name_or_addr)
             if isinstance(name_or_addr, MemoryAddress):
@@ -67,31 +95,30 @@ class MemoryBlock(VGroup):
             blackboard = self.heap_blackboard
         else:
             blackboard = self.stack_blackboard
-        var.align_to(blackboard, LEFT)
+        blackboard.add_var(var)
+        # var.to_edge(blackboard, movement.LEFT_EDGE)
+        # var.shift(movement.RIGHT * padding)
 
-        last_block = self.last_block.get(id(blackboard), None)
-        if last_block is None:
-            var.align_to(blackboard, UP)
-        else:
-            var.align_to(last_block, DOWN)
-            var.shift(DOWN * var.rect.height * 2)
-        self.last_block[id(blackboard)] = var
+        # last_block = self.last_block.get(blackboard, None)
+        # if last_block is None:
+        #     var.to_edge(blackboard, movement.TOP_EDGE)
+        #     var.shift(movement.DOWN * padding)
+        # else:
+        #     var.to_edge(last_block, movement.BOTTOM_EDGE)
+        #     var.shift(movement.DOWN * (var.box_height + padding))
+        # self.last_block[blackboard] = var
         self.vars_by_name[name] = var
 
-        if in_heap:
-            self.scene.play(Write(var))
-        blackboard.add(var)
+        # self.add(var)
 
     def delete_var(self, name, in_heap):
         if not in_heap:
             raise NotImplementedError()
         var = self.vars_by_name[name]
         self.vars_by_name.pop(name)
-
-        self.heap_blackboard.remove(var)
-        self.scene.play(Unwrite(var))
+        self.remove(var)
 
     def set_value(self, name, value):
         var = self.vars_by_name[name]
-        var.set_value(value, transition_animator=self.scene)
+        var.set_value(value)
 
