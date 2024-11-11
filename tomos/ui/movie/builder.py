988675aee56fe5ccaf090f464bef291265cc3e01
Javier Mansilla <javimansilla@gmail.com>
2024-11-11 00:20:00 -0300

    added logger

--- tomos/ui/movie/builder.py
+++ tomos/ui/movie/builder.py
@@ -1,18 +1,18 @@
+from logging import getLogger
 from pathlib import Path
 
 from tomos.ui.movie import configs
 from tomos.ui.movie.scene import TomosScene
 
+logger = getLogger(__name__)
+
 
 def build_movie(source_code_path, timeline, delay=0.5):
-
     output_path = Path.cwd() / "output_tomos" / Path(source_code_path).name
     output_path.mkdir(parents=True, exist_ok=True)
     scene = TomosScene(filename=source_code_path, timeline=timeline, output_path=output_path)
-    print("Rendering video")
-    print(len(scene.timeline.timeline))
-    # scene.render()
-    # open_media_file(scene.renderer.file_writer.movie_file_path)
+    logger.info(f"Rendering movie to {output_path}")
+    scene.render()
 
 
 if __name__ == '__main__':
