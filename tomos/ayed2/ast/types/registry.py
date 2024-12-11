7aa9eb3335f736c620a530b782618e5568f51d68
Javier Mansilla <javimansilla@gmail.com>
2024-12-11 16:29:34 -0300

    extended type registry with basic reset, and error detection features

--- tomos/ayed2/ast/types/registry.py
+++ tomos/ayed2/ast/types/registry.py
@@ -4,6 +4,11 @@ from .basic import IntType, RealType, BoolType, CharType
 
 class TypeRegistry:
     def __init__(self):
+        self.load_basic_types()
+        # kind of weird coded like this instead of directly in __init__
+        # it's useful for testing
+
+    def load_basic_types(self):
         self.type_map = {
             "int": IntType,
             "real": RealType,
@@ -11,7 +16,12 @@ class TypeRegistry:
             "char": CharType,
         }
 
+    def reset(self):
+        self.load_basic_types()
+
     def register_type(self, name, type_factory):
+        if name in self.type_map:
+            raise TomosTypeError(f"Type {name} is already registered.")
         self.type_map[name] = type_factory
 
     def get_type(self, name):
