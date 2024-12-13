0a86cfec991f6c1c2af631f93bee5d4c1960788e
Javier Mansilla <javimansilla@gmail.com>
2024-12-13 16:28:48 -0300

    Refactored TypeRegistry and Enum so it handles gracefully evaluation of enum-literals

--- tomos/ayed2/ast/types/enum.py
+++ tomos/ayed2/ast/types/enum.py
@@ -3,14 +3,17 @@ from tomos.exceptions import EnumerationError
 
 
 class Enum(Ayed2Type):
-    def __init__(self, values):
+    def __init__(self, constant_names):
         self.underlying_type = IntType
-        for value in values:
+        for value in constant_names:
             if not isinstance(value, str):
                 raise EnumerationError(
-                    f"Enum values must be strings, not {type(value)}"
+                    f"Enum constant_names must be strings, not {type(value)}"
                 )
-        self.values = values
+        self.constants = {
+            name: EnumConstant(self, name, value)
+            for value, name in enumerate(constant_names, start=1)
+        }
 
     def __call__(self):
         return self
@@ -24,4 +27,17 @@ class Enum(Ayed2Type):
         return self.underlying_type.is_pointer
 
     def is_valid_value(self, value):
-        return value in self.values
+        if isinstance(value, str):
+            return value in self.constants
+        if isinstance(value, EnumConstant):
+            return value in self.constants.values()
+
+
+class EnumConstant:
+    def __init__(self, enum, name, value):
+        self.enum = enum
+        self.name = name
+        self.value = value
+
+    def __repr__(self):
+        return f"{self.name}"
--- tomos/ayed2/ast/types/registry.py
+++ tomos/ayed2/ast/types/registry.py
@@ -3,6 +3,25 @@ from .basic import IntType, RealType, BoolType, CharType, Ayed2Type
 from .enum import Enum
 
 
+class EnumConstantsRegistry:
+    def __init__(self):
+        self.constant_mapping = {}
+
+    def get_overlap(self, new_constants_map):
+        current_names = set(self.constant_mapping.keys())
+        new_names = set(new_constants_map.keys())
+        return current_names.intersection(new_names)
+
+    def update(self, new_constants_map):
+        self.constant_mapping.update(new_constants_map)
+
+    def get_constant(self, name):
+        if not name in self.constant_mapping:
+            raise TomosTypeError(f"Unknown enum constant: {name}. Available constants are: {list(self.constant_mapping.keys())}")
+        return self.constant_mapping[name]
+
+
+
 class TypeRegistry:
     def __init__(self):
         # kind of weird coded like this instead of directly in __init__
@@ -19,7 +38,7 @@ class TypeRegistry:
 
     def reset(self):
         self.load_basic_types()
-        self._reserved_constant_names = set()
+        self._enum_constants = EnumConstantsRegistry()
 
     def register_type(self, name, new_type):
         if not isinstance(new_type, Ayed2Type):
@@ -27,11 +46,11 @@ class TypeRegistry:
         if name in self.type_map:
             raise TomosTypeError(f"Type {name} is already registered.")
         if isinstance(new_type, Enum):
-            if self._reserved_constant_names.intersection(new_type.values):
-                intersection = self._reserved_constant_names.intersection(new_type.values)
-                raise TomosTypeError(f"Type {name} is already registered and has the following constants: {intersection}")
+            overlap = self._enum_constants.get_overlap(new_type.constants)
+            if overlap:
+                raise TomosTypeError(f"Enum constants overlap: {overlap}")
             else:
-                self._reserved_constant_names.update(new_type.values)
+                self._enum_constants.update(new_type.constants)
         self.type_map[name] = new_type
 
     def get_type(self, name):
@@ -41,6 +60,9 @@ class TypeRegistry:
 
     def list_types(self):
         return list(self.type_map.items())
+
+    def get_enum_constant(self, name):
+        return self._enum_constants.get_constant(name)
 
 
 type_registry = TypeRegistry()  # Global type registry
