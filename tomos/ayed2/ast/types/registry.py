df879dfdadfc6d49297e358499153191423cd131
Javier Mansilla <javimansilla@gmail.com>
2024-12-13 14:57:37 -0300

    added several checks to typeregistry

--- tomos/ayed2/ast/types/registry.py
+++ tomos/ayed2/ast/types/registry.py
@@ -1,12 +1,13 @@
 from tomos.exceptions import TomosTypeError
-from .basic import IntType, RealType, BoolType, CharType
+from .basic import IntType, RealType, BoolType, CharType, Ayed2Type
+from .enum import Enum
 
 
 class TypeRegistry:
     def __init__(self):
-        self.load_basic_types()
         # kind of weird coded like this instead of directly in __init__
         # it's useful for testing
+        self.reset()
 
     def load_basic_types(self):
         self.type_map = {
@@ -18,16 +19,28 @@ class TypeRegistry:
 
     def reset(self):
         self.load_basic_types()
+        self._reserved_constant_names = set()
 
-    def register_type(self, name, type_factory):
+    def register_type(self, name, new_type):
+        if not isinstance(new_type, Ayed2Type):
+            raise TomosTypeError(f"Cant register type {new_type} because it does not inherit from Ayed2Type.")
         if name in self.type_map:
             raise TomosTypeError(f"Type {name} is already registered.")
-        self.type_map[name] = type_factory
+        if isinstance(new_type, Enum):
+            if self._reserved_constant_names.intersection(new_type.values):
+                intersection = self._reserved_constant_names.intersection(new_type.values)
+                raise TomosTypeError(f"Type {name} is already registered and has the following constants: {intersection}")
+            else:
+                self._reserved_constant_names.update(new_type.values)
+        self.type_map[name] = new_type
 
     def get_type(self, name):
         if name not in self.type_map:
             raise TomosTypeError(f"Unknown type: {name}. Available types are: {list(self.type_map.keys())}")
         return self.type_map[name]
+
+    def list_types(self):
+        return list(self.type_map.items())
 
 
 type_registry = TypeRegistry()  # Global type registry
