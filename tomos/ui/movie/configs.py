34f326c84b70e329c6dd1964ab04621af3d4da53
Javier Mansilla <javimansilla@gmail.com>
2025-02-10 12:36:31 -0300

    removed old commented lines

--- tomos/ui/movie/configs.py
+++ tomos/ui/movie/configs.py
@@ -1,15 +1,12 @@
 print('shall include here some import from ENV that allows to override these configs')
 
-# CANVAS_SIZE = (1920, 1080)
 CANVAS_SIZE = (1280, 720)
 CANVAS_COLOR = "#1A1A1D"
 
 MEMORY_BOARD_SIZE = (300, CANVAS_SIZE[1] * 0.9) # we'll have one for stack and one for heap
 PADDING = 12
 
-
 SCALE = 1
-# SCALE = 2
 
 # TEXTs
 BASE_FONT_SIZE = 20
@@ -35,5 +32,5 @@ COLOR_BY_TYPE = {
     "ArrayOf": "#F4D345",
     }
 
-
 UNNAMED_COLORS = ["red", "blue", "orange", "purple", "black", "pink"]
+
