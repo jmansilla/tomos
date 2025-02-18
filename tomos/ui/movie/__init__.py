6c7f61d089703bceafb5e9d140b3be811032ee05
Javier Mansilla <javimansilla@gmail.com>
2025-02-18 14:04:04 -0300

    ui settings by toml and b&d

--- tomos/ui/movie/__init__.py
+++ tomos/ui/movie/__init__.py
@@ -1,5 +1,5 @@
-from tomos.buttons_and_dials import ButtonsAndDialsSet
+from buttons_and_dials import ConfigSet
 from pathlib import Path
 
 here = Path(__file__).parent.resolve()
-configs = ButtonsAndDialsSet('tomos', here/'tomos.toml', check_cwd=False, argv_prefix='--cfg')
+configs = ConfigSet('tomos_ui', here/'tomos_ui.toml', check_cwd=False, argv_prefix='--cfg')
