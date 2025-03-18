f4165f808ce42367b51c13ef91ec718ee88f0897
Javier Mansilla <javimansilla@gmail.com>
2025-03-18 00:43:42 -0300

    minor fixes while working on report

--- tomos/ayed2/parser/__init__.py
+++ tomos/ayed2/parser/__init__.py
@@ -7,10 +7,8 @@ from .parsetree_to_ast import TreeToAST
 
 def get_grammar_txt():
     grammar_file = Path(__file__).parent.joinpath("grammar.lark")
-
     grammar_lines = open(grammar_file, "r").readlines()
     grammar_txt = "\n".join(l for l in grammar_lines if not l.startswith("//"))
-
     return grammar_txt
 
 
@@ -18,7 +16,6 @@ class TomosParser(Lark):
 
     def parse(self, *args, **kwargs):
         from tomos.ayed2.ast.types.registry import type_registry  # avoid circular import
-
         parse_results = super().parse(*args, **kwargs)
         type_registry.resolve_deferred_types()
         return parse_results
